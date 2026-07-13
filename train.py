494341ab3ef0b1c774c19cbc0679575293fc58c6
dikey4405 <167569079+dikey4405@users.noreply.github.com>
2026-07-13 09:40:06 +0700

    Update train.py

--- train.py
+++ train.py
@@ -321,8 +321,8 @@ class ABSATrainer:
 # ==========================================
 def main():
     # Cấu hình cứng
-    TRAIN_DATA = "Data/mobile_train.jsonl"
-    VAL_DATA = "Data/mobile_dev.jsonl"
+    TRAIN_DATA = "Data/restaurant_train.jsonl"
+    VAL_DATA = "Data/restaurant_dev.jsonl"
     BATCH_SIZE = 16
     MAX_LENGTH = 128
     EPOCHS = 15
